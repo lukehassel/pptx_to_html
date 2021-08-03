4dc68181ec28ad147a57be01f5e4e33a56367c07
Luke Hassel <52568027+lukehassel@users.noreply.github.com>
2021-08-03 17:20:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pptx_to_html-lukeehassel",
+    name="pptx_to_html",
     version="0.0.2",
     author="Example Author",
     author_email="lukeehassel@gmail.com",
